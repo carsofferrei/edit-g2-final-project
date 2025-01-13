6f09aed14b207aad7ac5dd242ceb3da1fd38f5d7
Carla Ferreira <carla.s.ferreira@MacBook-Pro-de-Carla.local>
2025-01-13 23:20:25 +0000

    Fact table and dim_date on dev folder

---
+++ src/models/dev/dim_date.sql
@@ -0,0 +1,12 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+-- Criar uma dim_date a partir de uma função dbt que está integrada numa package no dbt hubs
+WITH DATES as ({{ dbt_date.get_date_dimension("2025-01-01", "2099-12-31") }})
+
+SELECT *
+, current_timestamp as ingested_at
+FROM DATES 
--- src/models/dev/fact_historical_trips.sql
+++ src/models/dev/fact_historical_trips.sql
@@ -1,62 +1,116 @@
 {{
   config(
-    materialized = 'view',
-    )
+    materialized = 'view'
+  )
 }}
 
 WITH API_STOP AS (
-SELECT
-    id, 
-    lat, 
-    lon, 
-    CAST(line_id AS STRING) AS lines_id
+  SELECT
+    id
+    , lat
+    , lon
+    , stop_id
+    , CAST(line_id AS STRING) AS lines_id
   FROM 
-    `data-eng-dev-437916.data_eng_project_group2.api_stops`,
+    {{ source('data_eng_project_group2', 'api_stops') }},
     UNNEST(lines) AS line_id
 )
 
-, JOIN_STOPS_HIST AS (
-  SELECT DISTINCT A.*
-  , B.lat
-  , B.lon
-  , ROW_NUMBER() OVER (PARTITION BY trip_id ORDER BY timestamp) AS STOP_SEQUENCE
-  FROM (SELECT DISTINCT A.* FROM `data-eng-dev-437916.de_project_teachers.historical_stop_times` A)  A
-  LEFT JOIN (SELECT * FROM `data-eng-dev-437916.data_eng_project_group2.api_stops`, 
-    UNNEST(lines) AS line_id) B
-ON A.stop_id = B.stop_id AND CAST(A.line_id AS STRING)=B.line_id
-WHERE A.trip_id = '2006_0_3|2|1|1100_5ZVUM'
-) 
-,
-distances AS (
+, HIST_STOP_TIMES AS (
   SELECT
-    trip_id,
-    stop_id,
-    line_id,
-    stop_sequence,
-    current_status,
-    lat,
-    lon,
-    LAG(lat) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS prev_lat,
-    LAG(lon) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS prev_lon
-  FROM
-    JOIN_STOPS_HIST
+      *
+    , TIMESTAMP_SECONDS(timestamp) AS timestamp_UTC
+    , DATE(TIMESTAMP_SECONDS(timestamp)) AS trip_dat
+  FROM 
+      {{ source('de_project_teachers', 'historical_stop_times') }}
 )
 
-SELECT
-  trip_id,
-  stop_sequence,
-  stop_id,
-  line_id,
-  current_status,
-  IF(prev_lat IS NOT NULL AND prev_lon IS NOT NULL,
-     ST_DISTANCE(ST_GEOGPOINT(prev_lon, prev_lat), ST_GEOGPOINT(lon, lat)) / 1000,
-     0
-  ) AS distance_between_stops_km,
-  SUM(IF(prev_lat IS NOT NULL AND prev_lon IS NOT NULL,
-          ST_DISTANCE(ST_GEOGPOINT(prev_lon, prev_lat), ST_GEOGPOINT(lon, lat)) / 1000,
-          0
-      )) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS cumulative_distance_km
-FROM
-  distances
-ORDER BY
-  trip_id, stop_sequence
+, JOIN_STOPS_HIST AS (
+  SELECT 
+    DISTINCT A.*
+    , B.lat
+    , B.lon
+    , ROW_NUMBER() OVER (PARTITION BY trip_id ORDER BY timestamp_UTC) AS stop_sequence
+  FROM HIST_STOP_TIMES A
+  LEFT JOIN API_STOP B
+  ON A.stop_id = B.stop_id AND CAST(A.line_id AS STRING) = B.lines_id
+)
+
+, DISTANCE AS (
+  SELECT
+    trip_id
+    , stop_id
+    , line_id
+    , stop_sequence
+    , current_status
+    , trip_dat
+    , lat
+    , lon
+    , LAG(lat) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS prev_lat
+    , LAG(lon) OVER (PARTITION BY trip_id ORDER BY stop_sequence) AS prev_lon
+  FROM JOIN_STOPS_HIST
+)
+
+, DISTANCE_FINAL AS (
+  SELECT
+      trip_id
+      , stop_sequence
+      , stop_id
+      , line_id
+      , current_status
+      , trip_dat
+      , ROUND(
+        CASE 
+          WHEN prev_lat IS NOT NULL AND prev_lon IS NOT NULL 
+          THEN ST_DISTANCE(ST_GEOGPOINT(prev_lon, prev_lat), ST_GEOGPOINT(lon, lat)) / 1000
+          ELSE 0
+          END, 3) AS distance_between_stops_km
+      , ROUND(
+          SUM(CASE 
+                WHEN prev_lat IS NOT NULL AND prev_lon IS NOT NULL 
+                THEN ST_DISTANCE(ST_GEOGPOINT(prev_lon, prev_lat), ST_GEOGPOINT(lon, lat)) / 1000
+                ELSE 0
+                END) OVER (PARTITION BY trip_id ORDER BY stop_sequence)
+              , 3) AS cumulative_distance_km
+  FROM DISTANCE
+  ORDER BY trip_id, stop_sequence
+)
+
+, HIST_STOP_TIMES_TRIP AS (
+  SELECT
+    line_id
+    , trip_id
+    , trip_dat
+    , MIN(timestamp_UTC) AS begin_trip
+    , MAX(timestamp_UTC) AS end_trip
+    , COUNT(DISTINCT stop_id) AS trip_stops
+  FROM HIST_STOP_TIMES
+  GROUP BY line_id, trip_id, trip_dat
+)
+
+, AUX_FINAL AS (
+  SELECT 
+      DISTANCE_FINAL.trip_id
+    , DISTANCE_FINAL.line_id
+    , DISTANCE_FINAL.trip_dat
+    , HIST_STOP_TIMES_TRIP.trip_stops
+    , MAX(cumulative_distance_km) AS trip_total_distance_km
+    , TRUNC(TIMESTAMP_DIFF(end_trip, begin_trip, SECOND) / 60) AS trip_total_time_min
+  FROM DISTANCE_FINAL
+  LEFT JOIN HIST_STOP_TIMES_TRIP
+  ON DISTANCE_FINAL.trip_id = HIST_STOP_TIMES_TRIP.trip_id AND DISTANCE_FINAL.line_id = HIST_STOP_TIMES_TRIP.line_id
+  GROUP BY DISTANCE_FINAL.trip_id, DISTANCE_FINAL.line_id, DISTANCE_FINAL.trip_dat, HIST_STOP_TIMES_TRIP.trip_stops, TRUNC(TIMESTAMP_DIFF(end_trip, begin_trip, SECOND) / 60) 
+)
+
+SELECT  
+    trip_id
+    , line_id
+    , trip_dat
+    , trip_stops
+    , trip_total_distance_km
+    , trip_total_time_min
+    , ROUND(CASE
+            WHEN trip_total_time_min > 0 THEN trip_total_distance_km / trip_total_time_min
+            ELSE NULL
+            END, 3) AS trip_avg_speed
+FROM AUX_FINAL
