bae9b01d4b26a028c56e4bfd8e9a3a21a41f7ed0
rutevicente <rute.s.v@protonmail.com>
2025-01-14 23:52:06 +0000

    Added tables (dim_lines, dim_stops). Updated dev schema.

---
+++ src/models/dev/dim_lines.sql
@@ -0,0 +1,45 @@
+
+  {{
+  config(
+    materialized = 'view',
+    )
+}}
+
+
+WITH 
+    expanded_data AS ( -- Expands the arrays MUNICIPALITIES and ROUTES
+        SELECT DISTINCT
+            id AS line_id,
+            long_name AS line_name,
+            route_id AS route_id
+        FROM {{ source('data_eng_project_group2', 'api_lines') }}, 
+            UNNEST(patterns) AS pattern_id,
+            UNNEST(routes) AS route_id
+    ),
+
+    routes_joined AS (
+        SELECT
+            A.route_id,
+            B.long_name AS route_name,
+            A.line_id,
+            A.line_name
+        FROM expanded_data A
+        LEFT JOIN {{ source('data_eng_project_group2', 'api_routes') }} B 
+            ON A.route_id = B.id
+    ),
+
+    final_result AS (
+        SELECT 
+            B.trip_id,
+            A.line_id,
+            A.line_name,
+            A.route_id,
+            A.route_name,
+            B.direction_id
+        FROM routes_joined A
+        LEFT JOIN {{ source('data_eng_project_group2', 'api_trips') }} B 
+            ON A.route_id = B.route_id
+    )
+
+SELECT *
+FROM final_result
---
+++ src/models/dev/dim_stops.sql
@@ -0,0 +1,16 @@
+  {{
+  config(
+    materialized = 'view',
+    )
+}}
+
+
+
+SELECT
+    stop_id,
+    name AS stop_name,
+    municipality_id,
+    municipality_name,
+    region_name,
+    operational_status
+FROM {{ source('data_eng_project_group2', 'api_stops') }}
