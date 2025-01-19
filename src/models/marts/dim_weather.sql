8339355abe25fc6e21f7e2b6e9731f28ce67c154
Carla Ferreira <csofiaf.6@gmail.com>
2025-01-19 23:18:06 +0000

    Create dim_weather

---
+++ src/models/marts/dim_weather.sql
@@ -0,0 +1,14 @@
+  {{
+  config(
+    materialized = 'table',
+    )
+}}
+
+SELECT DISTINCT
+  unnested_field.dataPrevisao as weather_date
+, unnested_field.tMin as min_temperature
+, unnested_field.tMax as max_temperature
+, unnested_field.rumoPredVento as wind_direction
+, unnested_field.probPrecipita as rain_probability
+FROM {{ source('data_eng_project_group2', 'api_weather_forecast_cleaned') }},
+UNNEST(data) as unnested_field
