55e66059995b50d5a311fcf463ff70ce85811cf2
Carla Ferreira <csofiaf.6@gmail.com>
2025-01-19 23:40:24 +0000

    Change dim_routes model and change yaml documentation for marts. DBT test validate all tests and all pass

--- src/models/marts/dim_routes.sql
+++ src/models/marts/dim_routes.sql
@@ -7,20 +7,24 @@
 {% set surrogate_key_columns = ['A.route_id'] %}
 
 SELECT {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }} as sk_route,
-   A.route_id
-  , A.line_id
-  , ARRAY_AGG(CAST(A.municipality_id AS STRING)) as municipality_id
-  , ARRAY_AGG(A.municipality_name) as municipality_name
-  , A.route_name
-  , ARRAY_AGG(CAST(B.stop_id AS STRING)) as stop_id
-  , ARRAY_AGG(B.stop_name) as stop_name
-  , ARRAY_AGG(B.region_name) as region_name
-  , ARRAY_AGG(B.operational_status) AS stop_operational_status
-  , current_timestamp as ingested_at
+   A.route_id,
+   A.line_id,
+   ARRAY_AGG(STRUCT(
+       CAST(A.municipality_id AS STRING) AS municipality_id,
+            A.municipality_name AS municipality_name
+   )) AS municipalities,
+   A.route_name,
+   ARRAY_AGG(STRUCT(
+       CAST(B.stop_id AS STRING) AS stop_id,
+            B.stop_name AS stop_name,
+            B.region_name AS region_name,
+            B.operational_status AS stop_operational_status
+   )) AS stops,
+   current_timestamp AS ingested_at
 FROM {{ ref('stg_routes') }} A
 LEFT JOIN {{ ref('stg_stops') }} B
-  ON A.route_id = B.route_id 
-GROUP BY 
+  ON A.route_id = B.route_id
+GROUP BY
   {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
   , A.route_id
   , A.line_id
