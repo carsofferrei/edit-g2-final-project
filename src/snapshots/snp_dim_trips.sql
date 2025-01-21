d7132f6898f62761598e797c55fcf0cf6c1394ae
Carla Ferreira <csofiaf.6@gmail.com>
2025-01-21 19:56:58 +0000

    Add the snapshots to a dataset

--- src/snapshots/snp_dim_trips.sql
+++ src/snapshots/snp_dim_trips.sql
@@ -1,7 +1,7 @@
 {% snapshot snp_dim_trips %}
 {{
   config(
-     target_schema='data_eng_project_group2_marts',
+     target_schema='data_eng_project_group2_snapshots',
      unique_key='sk_trip',
      strategy='check',
      check_cols=['line_id', 'line_name', 'route_id', 'route_name', 'direction_id', 'direction_name']
