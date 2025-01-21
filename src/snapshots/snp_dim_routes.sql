796ecc4c420b11300269277e96d725576f79eb32
Carla Ferreira <csofiaf.6@gmail.com>
2025-01-21 23:00:55 +0000

    Add snapshots documentation, tests and snp_dim_routes

---
+++ src/snapshots/snp_dim_routes.sql
@@ -0,0 +1,14 @@
+{% snapshot snp_dim_routes %}
+{{
+  config(
+     target_schema='data_eng_project_group2_snapshots',
+     unique_key='sk_route',
+     strategy='check',
+     check_cols=['route_id', 'line_id', 'route_id', 'municipalities', 'route_name', 'stops']
+   )
+}}
+
+SELECT * 
+FROM {{ ref('dim_routes') }}
+
+{% endsnapshot %}
--- src/snapshots/snp_dim_trips.sql
+++ src/snapshots/snp_dim_trips.sql
@@ -8,38 +8,7 @@
    )
 }}
 
-{% set surrogate_key_columns = ['trip_id'] %}
-
-WITH routes_joined AS (
-    SELECT 
-        A.route_id,
-        B.route_name,
-        A.line_id,
-        A.line_name
-    FROM {{ ref('stg_lines') }} A
-    LEFT JOIN {{ ref('stg_routes') }} B 
-      ON A.route_id = B.route_id
-),
-
-final_result AS (
-    SELECT 
-        {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }} AS sk_trip,
-        B.trip_id,
-        B.service_id,
-        A.line_id,
-        A.line_name,
-        A.route_id,
-        A.route_name,
-        B.direction_id,
-        B.direction_name
-    FROM routes_joined A
-    LEFT JOIN {{ ref('stg_trips') }} B 
-      ON A.route_id = B.route_id
-)
-
-SELECT DISTINCT *
-      , {{ var('truncate_timespan_to') }}  as ingested_at
-FROM final_result
-
+SELECT * 
+FROM {{ ref('dim_trips') }}
 
 {% endsnapshot %}
