279e0e3ac9c58718e039ec724d20cbba195438db
Carla Ferreira <csofiaf.6@gmail.com>
2025-01-23 22:48:35 +0000

    Final snp_dim_routes and queries for testing the schema

---
+++ queries_testing_models/queries.sql
@@ -0,0 +1,113 @@
+--- Partilhar visão da dim snapshot para uma rota:
+SELECT *
+FROM `data-eng-dev-437916.data_eng_project_group2_snapshots.snp_dim_routes`
+WHERE sk_route = 'a1ee4cebffbe0450d0eb87ff331050a9'
+ORDER BY dbt_valid_from asc;
+--- Para uma paragem:
+SELECT *
+FROM `data-eng-dev-437916.data_eng_project_group2_snapshots.snp_dim_routes`
+WHERE CONTAINS_SUBSTR(stops, 'R Manuel Casimiro Q1')
+
+
+--- No dia 18/01, quantas viagens feitas passaram na R Manuel Casimiro Q1?
+WITH STOP AS (
+    SELECT *
+    FROM 
+      `data-eng-dev-437916.data_eng_project_group2_marts.dim_routes`, 
+      UNNEST(stops) AS stop,
+      UNNEST(municipalities) AS municipalities
+    WHERE 
+      stop.stop_name LIKE 'R Manuel Casimiro Q1'
+      AND dbt_valid_to IS NULL
+)
+
+SELECT trip_date
+, STOP.route_name
+, COUNT(DISTINCT STOP.line_id) AS NR_LINHAS
+, COUNT(DISTINCT STOP.municipality_name) AS NR_MUNICIPIOS
+, COUNT(DISTINCT sk_trip) AS NR_VIAGENS
+, SUM(total_trip_distance) AS total_trip_distance
+, SUM(total_trip_time) AS total_trip_time
+, SUM(total_trip_distance) / SUM(total_trip_time) AS avg_speed
+FROM `data-eng-dev-437916.data_eng_project_group2_marts.fact_historical_trips` A
+INNER JOIN STOP
+ON A.sk_route = STOP.sk_route
+WHERE A.trip_date >= '2025-01-18' AND A.trip_date <= CURRENT_DATE()
+GROUP BY trip_date
+, STOP.route_name
+ORDER BY STOP.route_name, A.trip_date;
+
+
+-- Rotas que param em estação X
+SELECT 
+  route_id,
+  route_name,
+  stop.stop_id,
+  stop.stop_name,
+  stop.stop_operational_status
+FROM `data-eng-dev-437916.data_eng_project_group2_marts.dim_routes`,
+UNNEST(stops) AS stop
+WHERE stop.stop_id = '80084'
+
+
+-- Rotas que servem munícipio X 
+SELECT DISTINCT 
+    m.municipality_name, 
+    r.route_id, 
+    r.route_name, 
+FROM 
+    `data-eng-dev-437916.data_eng_project_group2_marts.dim_routes` r,
+    UNNEST(municipalities) AS m
+WHERE 
+    m.municipality_name = 'Mafra';
+
+
+-- As duas questões anteriores mas com algum detalhe adicional na filtragem
+WITH dim_routes AS (
+  SELECT 
+    sk_route,
+    route_id,
+    route_name,
+    line_id,
+    m.municipality_name,
+    stop.stop_id,
+    stop.stop_name
+  FROM `data-eng-dev-437916.data_eng_project_group2_marts.dim_routes`,
+  UNNEST(stops) AS stop,
+  UNNEST(municipalities) AS m
+),
+
+dim_dates AS (
+  SELECT *
+  FROM `data-eng-dev-437916.data_eng_project_group2_marts.dim_calendar_dates` d
+),
+
+dim_trips AS (
+  SELECT 
+    sk_trip,
+    direction_id,
+    direction_name
+  FROM `data-eng-dev-437916.data_eng_project_group2_marts.dim_trips` t
+)
+
+
+SELECT DISTINCT
+  r.line_id
+  ,r.route_id
+  ,r.route_name
+  ,d.day_type
+  --,t.direction_id
+  --,r.stop_id
+  --,r.stop_name
+  --,r.municipality_name
+  --,f.total_trip_distance
+  --,f.average_trip_speed
+FROM `data-eng-dev-437916.data_eng_project_group2_marts.fact_historical_trips` f
+INNER JOIN dim_routes r ON f.sk_route = r.sk_route
+INNER JOIN dim_dates d ON f.trip_date = d.date
+--INNER JOIN dim_trips t ON f.sk_trip = t.sk_trip
+WHERE r.line_id = '1005' -- Em que day_type circula a linha 1005?
+--WHERE r.stop_id= '80084' AND d.day_type = 3 -- day_type 3 = fim de semana | Rotas que passam na estação X ao fim de semana
+--WHERE r.municipality_name = 'Loures' AND d.period_name = 'Verão'  -- Rotas que servem município X durante o período de Verão
+--WHERE r.municipality_name = 'Sintra' AND t.direction_id = 1 -- Rotas que servem o município x com direction_id = 1 (Outbound)
+--WHERE r.municipality_name = 'Lisboa' AND f.trip_date = '2024-01-22' -- Rotas que circularam na passada quarta-feira, no município X
--- src/snapshots/snp_dim_routes.sql
+++ src/snapshots/snp_dim_routes.sql
@@ -1,14 +1,43 @@
 {% snapshot snp_dim_routes %}
 {{
   config(
-     target_schema='data_eng_project_group2_snapshots',
-     unique_key='sk_route',
-     strategy='check',
-     check_cols=['route_id', 'municipalities', 'route_name', 'stops']
-   )
+    target_schema='data_eng_project_group2_snapshots',
+    unique_key='sk_route',
+    strategy='check',
+    check_cols=['route_id', 'route_name', 'municipalities', 'stops_str']
+  )
 }}
 
-SELECT * 
+SELECT 
+    sk_route,
+    route_id,
+    line_id,
+    route_name,
+    ARRAY_TO_STRING(
+        ARRAY(
+            SELECT TO_JSON_STRING(STRUCT(
+                CAST(municipality_id AS STRING) AS municipality_id,
+                municipality_name AS municipality_name
+            ))
+            FROM UNNEST(municipalities)
+            ORDER BY municipality_id
+        ),
+        ','
+    ) AS municipalities,
+    ARRAY_TO_STRING(
+        ARRAY(
+            SELECT TO_JSON_STRING(STRUCT(
+                CAST(stop_id AS STRING) AS stop_id,
+                stop_name AS stop_name,
+                region_name AS region_name,
+                stop_operational_status AS stop_operational_status
+            ))
+            FROM UNNEST(stops)
+            ORDER BY stop_id
+        ),
+        ','
+    ) AS stops,
+    ingested_at
 FROM {{ ref('dim_routes') }}
 
 {% endsnapshot %}
